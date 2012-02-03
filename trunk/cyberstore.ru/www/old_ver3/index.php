413106561044048090c79e74317ef80ba5f659bd
overmind1001@gmail.com <overmind1001@gmail.com@3422b5e4-9365-fb06-c293-7bd22532eeee>
2012-02-03 16:03:53 +0000

    4 версия

---
+++ trunk/cyberstore.ru/www/old_ver3/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+	<title>CyberStore - все для киборгов</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	
+</head>
+<body>
+	<div class="head">
+		<div class="logo">
+			<!--<img src="logo.gif">-->
+			Лого
+		</div>
+		<div class="top_menu">
+			Верхнее менююююююю ююююююююююююю ююююююю
+		</div>
+	</div>
+	
+	<div class="body1">
+		<div class="left_menu">
+			Навигация
+		</div>
+		<div class="common">
+			Гланая информация
+			<p>dfg
+			<p>df
+			<p>df
+			<p>gd
+			<p>f
+			<p>g
+			<p>df
+			<p>df
+			<p>gd
+			<p>f
+			<p>g
+			
+			
+			
+		</div>
+		
+	</div>
+		<div class="footer">
+		Копирайт
+		<div>
+	
+
+</body>
+</html>
