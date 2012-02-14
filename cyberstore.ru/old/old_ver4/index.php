8b6ddebcbb8a7bd378942d54e73078897d4e29eb
overmind1001@gmail.com <overmind1001@gmail.com@3422b5e4-9365-fb06-c293-7bd22532eeee>
2012-02-14 16:40:15 +0000

    old4

---
+++ cyberstore.ru/old/old_ver4/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+	<title>CyberStore - все для киборгов</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	
+</head>
+<body>
+	<div class="header">
+		<div class="logo">
+			<img src="logo.gif">
+		</div>
+		<div class="top_menu">
+			<ul class="ul.top_menu">
+				<li class="top_menu">Корзина </li>
+				<li class="top_menu">Вход в систему</li>
+			</ul>
+		</div>
+	</div>
+	
+	<div class="left">
+		<ul>
+			<li>Главная 
+			<?php
+				for($i=0;$i<2;$i++)
+				{
+					echo "<Font color=red size=6em>Hello</Font>"; 
+				}
+			?>
+			
+			</li>
+			<li>Новости</li>
+			<li>Каталог</li>
+		</ul>
+	</div>
+	<div class="content">
+		<p>Гланая информация</p>
+		<p>Много текста с картинками</p>
+		<p>Много текста с картинками</p>
+		<p>Много текста с картинками</p>
+		<p>Много текста с картинками</p>
+		<p>Много текста с картинками</p>
+		<p>Много текста с картинками</p>
+	
+	</div>
+		
+	<div class="footer">
+	Копирайт
+	</div>
+	
+
+</body>
+</html>
