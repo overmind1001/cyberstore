f1477745984254fc51abb3df0eded83d405c3bcd
overmind1001@gmail.com <overmind1001@gmail.com@3422b5e4-9365-fb06-c293-7bd22532eeee>
2012-02-20 16:48:54 +0000

    Начал делать Админку

---
+++ cyberstore.ru/www/admin/goodsMain.php
@@ -0,0 +1,35 @@
+<form >
+    <table>
+        <tr><!--Заголовок-->
+            <h1>Товары</h1>
+        </tr>
+        
+        <tr><!--Лист-->
+            <select name="listGoods" size="30" required style="width: 500;">
+                <?php
+                    for($i=0;$i<100;$i++)
+                    {
+                        echo "<option>Товар $i</option>";
+                    }
+                ?>
+            </select>
+        </tr>
+        
+        <tr><!--Кнопки-->
+            <td>
+                <input name="btnAdd" type="button" value="Добавить товар" />
+            </td>
+            <td>
+                <input name="btnEdit" type="button" value="Редактировать товар" />
+            </td>
+            <td>
+                <input name="btnDelete" type="button" value="Удалить товар" />
+            </td>
+        </tr>
+    </table>
+</form>
+
+<?php
+
+
+?>
