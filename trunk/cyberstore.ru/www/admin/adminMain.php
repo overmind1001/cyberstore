9eb2720bb0d099daff84cb327c4fceff07404628
overmind1001@gmail.com <overmind1001@gmail.com@3422b5e4-9365-fb06-c293-7bd22532eeee>
2012-02-25 13:51:55 +0000

    Adminka: Еще понаписал несколько файлов. Реализации пока нет. Только формы.

---
+++ trunk/cyberstore.ru/www/admin/adminMain.php
@@ -0,0 +1,15 @@
+<h1>Админка</h1>
+<ul>
+    <li>
+        <a href="goodsMain.php">Товары</a>
+    </li>
+    <li>
+        <a href="div_partMain.php">Разделы каталога</a>
+    </li>
+    <li>
+        <a href="usersMain.php">Пользователи</a>
+    </li>
+    <li>
+        <a href="salesMain.php">Продажи</a>
+    </li>
+</ul>
