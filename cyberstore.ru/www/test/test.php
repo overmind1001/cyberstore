61e6f8df20279a83fb6f7b241f6c41019f7993ab
overmind1001@gmail.com <overmind1001@gmail.com@3422b5e4-9365-fb06-c293-7bd22532eeee>
2012-02-26 18:45:57 +0000

    Adminka: Еще понаписал несколько файлов. Реализации пока нет. Только формы.

---
+++ cyberstore.ru/www/test/test.php
@@ -0,0 +1,14 @@
+<?php
+// Include the main Propel script
+include_once '../initPropel.php';
+//require_once 'U:/usr/local/php5/PEAR/propel/Propel.php';
+// Initialize Propel with the runtime configuration
+Propel::init("../bookstore/build/conf/bookstore-conf.php");
+// Add the generated 'classes' directory to the include path
+set_include_path("../bookstore/build/classes" . PATH_SEPARATOR . get_include_path());
+
+$author = new Author();
+$author->setFirstName('Jane');
+$author->setLastName('Austen');
+$author->save();
+?>
