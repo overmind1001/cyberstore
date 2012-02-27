8d47017f6d00a20e71fabecdbf94bcb294f51344
overmind1001@gmail.com <overmind1001@gmail.com@3422b5e4-9365-fb06-c293-7bd22532eeee>
2012-02-27 15:04:56 +0000

    Propel: немного поднастроил

--- trunk/cyberstore.ru/www/initPropel.php
+++ trunk/cyberstore.ru/www/initPropel.php
@@ -1,3 +1,4 @@
 <?php
-    require_once 'U:/usr/local/php5/PEAR/propel/Propel.php';
+    //require_once 'U:/usr/local/php5/PEAR/propel/Propel.php';
+    require_once 'vendor/propel/runtime/lib/Propel.php';
 ?>
--- trunk/cyberstore.ru/www/test/test.php
+++ trunk/cyberstore.ru/www/test/test.php
@@ -1,4 +1,4 @@
-<?php
+<!--?php
 // Include the main Propel script
 include_once '../initPropel.php';
 //require_once 'U:/usr/local/php5/PEAR/propel/Propel.php';
@@ -11,4 +11,21 @@ $author = new Author();
 $author->setFirstName('Jane');
 $author->setLastName('Austen');
 $author->save();
+?-->
+
+<?php
+// Include the main Propel script
+include_once '../initPropel.php';
+//require_once 'U:/usr/local/php5/PEAR/propel/Propel.php';
+// Initialize Propel with the runtime configuration
+Propel::init("../cyberstore/build/conf/cyberstore-conf.php");
+// Add the generated 'classes' directory to the include path
+set_include_path("../cyberstore/build/classes" . PATH_SEPARATOR . get_include_path());
+
+$user = new User();
+$user->setLogin("alex3");
+$user->setPassword("alex3");
+$user->setId(3);
+$user->save();
+
 ?>
