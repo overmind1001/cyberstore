07eddda261e9904dd33753f7fcabc6b88df03f87
antonstorozhev@gmail.com <antonstorozhev@gmail.com@3422b5e4-9365-fb06-c293-7bd22532eeee>
2012-02-28 18:17:09 +0000

    Experimental: локальные версии библиотек, убрал пункт меню "новости"

--- trunk/cyberstore.ru/www/experimental/catalog.php
+++ trunk/cyberstore.ru/www/experimental/catalog.php
@@ -3,8 +3,8 @@
 	<head> 
 	<title>CyberStore - всё для киборгов</title> 
 	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0.1/jquery.mobile-1.0.1.min.css" />
-	<script src="http://code.jquery.com/jquery-1.6.4.min.js"></script>
-	<script src="http://code.jquery.com/mobile/1.0.1/jquery.mobile-1.0.1.min.js"></script>
+	<script src="jquery-1.6.4.min.js"></script>
+	<script src="jquery.mobile-1.0.1.min.js"></script>
 	<style>
 		.ui-page
 		{
@@ -55,8 +55,7 @@
 				<td valign="bottom">
 					<div data-role="navbar">
 						<ul>
-							<li><a href="index.php">Главная</a></li>
-							<li><a href="#">Новости</a></li>
+							<li><a href="./index.php">Главная</a></li>
 							<li><a href="#" class="ui-btn-active">Каталог</a></li>
 							<li><a href="#">Помощь</a></li>
 						</ul>
@@ -67,24 +66,10 @@
 
 	<div data-role="content">
 					<div id="catalog">
-						<table>
-							<!--tr>
-								<td>
-								</td>
-								<td>
-									<div style="text-align:center;"> 
-										<div data-role="controlgroup" data-type="horizontal" class="localnav">
-											<a href="#" data-role="button" data-inline="true" class="ui-btn-active">1</a>
-											<a href="#" data-role="button" data-inline="true">2</a>
-											<a href="#" data-role="button" data-inline="true">3</a>
-										</div>
-									</div>
-								</td>
-							</tr-->
+						<table border="0px">
 							<tr>
-								<td style="width:10%; vertical-align:top;">
+								<td style="width:10%; vertical-align:top;" rowspan="3">
 									<div data-role="collapsible-set" data-theme="c" data-content-theme="d">
-
 										<div data-role="collapsible" data-collapsed="false">
 										<h3>Категория 1</h3>
 										<ul data-role="listview">
@@ -120,7 +105,33 @@
 
 									</div>
 								</td>
-								<td style="padding-left:10px; padding-top:6px;">
+								<td width="13%" align="center">
+									<table>
+										<tr>
+											<td>
+												<!--label for="select-count" class="select">На странице: </label-->
+											</td>
+											<td>
+												<select id="select-count" name="select-choice-1" data-native-menu="false">
+													<option value="six">6</option>
+													<option value="twelte">12</option>
+													<option value="twentyfour">24</option>
+													<option value="fortyeight">48</option>
+												</select>
+											</td>
+										</tr>
+									</table>
+								</td>
+								<td align="center">
+									<div data-role="controlgroup" data-type="horizontal" class="localnav">
+										<a href="#" data-role="button" data-inline="true" class="ui-btn-active">1</a>
+										<a href="#" data-role="button" data-inline="true">2</a>
+										<a href="#" data-role="button" data-inline="true">3</a>
+									</div>
+								</td>
+							</tr>
+							<tr>
+								<td style="padding-left:10px; padding-top:6px;" colspan="2">
 									<div class="ui-grid-b">
 										<div class="ui-block-a">
 											<div class="ui-bar ui-bar-c" style="height:200px">
@@ -156,7 +167,8 @@
 								</td>
 							</tr>
 							<tr>
-								<td></td>
+								<td>
+								</td>
 								<td>
 									<div style="text-align:center;"> 
 										<div data-role="controlgroup" data-type="horizontal" class="localnav">
--- trunk/cyberstore.ru/www/experimental/index.php
+++ trunk/cyberstore.ru/www/experimental/index.php
@@ -61,8 +61,7 @@
 					<div data-role="navbar">
 						<ul>
 							<li><a href="#" class="ui-btn-active">Главная</a></li>
-							<li><a href="#">Новости</a></li>
-							<li><a href="catalog.php">Каталог</a></li>
+							<li><a href="./catalog.php">Каталог</a></li>
 							<li><a href="#">Помощь</a></li>
 						</ul>
 					</div>
