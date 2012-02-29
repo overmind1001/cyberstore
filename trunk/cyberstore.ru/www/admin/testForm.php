2e69cf71622456399b361c56359fe39b5b52e848
overmind1001@gmail.com <overmind1001@gmail.com@3422b5e4-9365-fb06-c293-7bd22532eeee>
2012-02-29 15:19:49 +0000

    Админка: Форма юзверей.

---
+++ trunk/cyberstore.ru/www/admin/testForm.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title></title>
+	<meta name="keywords" content="" />
+	<meta name="description" content="" />
+        
+        <!--
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.1.0-rc.1/jquery.mobile-1.1.0-rc.1.min.css" />
+	<script src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
+	<script src="http://code.jquery.com/mobile/1.1.0-rc.1/jquery.mobile-1.1.0-rc.1.min.js"></script>
+        -->
+        <link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" rel="stylesheet" type="text/css"/>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
+        <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
+        
+        <style>
+            * {
+                    margin: 0;
+                    padding: 0;}
+            html {
+                    height: 100%;}
+            body {
+                    font: 12px/18px Arial, Tahoma, Verdana, sans-serif;
+                    width: 100%;
+                    height: 100%;}
+            p {
+                    margin: 0 0 18px}
+            #wrapper {
+                    width: 1000px;
+                    margin: 0 auto;
+                    min-height: 100%;
+                    height: auto !important;
+                    height: 100%;}
+            /* Header
+            -----------------------------------------------------------------------------*/
+            #header {
+                    height: 150px;
+                    background: #FFE680;}
+            /* Middle
+            -----------------------------------------------------------------------------*/
+            #content {
+                    padding: 0 0 100px;}
+            /* Footer
+            -----------------------------------------------------------------------------*/
+            #footer {
+                    width: 1000px;
+                    margin: -100px auto 0;
+                    height: 100px;
+                    background: #BFF08E;
+                    position: relative;}
+        </style>
+</head>
+
+<body>
+
+<div id="wrapper">
+	<div id="header">	
+	</div><!-- #header-->
+	<div id="content">	
+            <ul id="selectable">
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 4</li>
+            <li>Item 5</li>
+            </ul>
+	</div><!-- #content-->
+</div><!-- #wrapper -->
+
+<div id="afooter">
+</div><!-- #footer -->
+
+</body>
+</html>
