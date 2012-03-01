6befd7e1310a89624122e8b00155e66357ea44fa
overmind1001@gmail.com <overmind1001@gmail.com@3422b5e4-9365-fb06-c293-7bd22532eeee>
2012-03-01 15:39:14 +0000

    Админка: Сделал главную для админки.

--- trunk/cyberstore.ru/www/admin/adminMain.php
+++ trunk/cyberstore.ru/www/admin/adminMain.php
@@ -1,15 +1,25 @@
+<?php
+    include 'adminHead.php';
+    $name="Админка";
+    generateHead($name, $name)
+?>
 <h1>Админка</h1>
-<ul>
-    <li>
+
+<ul style="padding-top:30px;font-size: 20pt;">
+    <li style="margin:20px;">
         <a href="goodsMain.php">Товары</a>
     </li>
-    <li>
+    <li  style="margin:20px;">
         <a href="div_partMain.php">Разделы каталога</a>
     </li>
-    <li> 
+    <li style="margin:20px;"> 
         <a href="users/">Пользователи</a>
     </li>
-    <li>
+    <li style="margin:20px;">
         <a href="salesMain.php">Продажи</a>
     </li>
 </ul>
+
+<?php
+    include 'adminFoot.php';
+?>
