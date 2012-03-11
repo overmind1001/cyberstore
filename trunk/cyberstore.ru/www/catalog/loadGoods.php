fccdfcfc8ca619afa744d57193d685dad7b23dcc
antonstorozhev@gmail.com <antonstorozhev@gmail.com@3422b5e4-9365-fb06-c293-7bd22532eeee>
2012-03-11 17:34:22 +0000

    Каталог: определяет, есть ли уже товар в корзине, чтоб второй раз не добавлять

--- trunk/cyberstore.ru/www/catalog/loadGoods.php
+++ trunk/cyberstore.ru/www/catalog/loadGoods.php
@@ -4,6 +4,7 @@
     * @param categoryId - id запрашиваемой категории
     * @param count - запрашиваемое кол-во товаров (6, 12, 24, 48)
     * @param skip - сколько товаров надо пропустить (для загрузки второй и последующих страниц)
+    * @param sessionId - id текущей сессии
     * @returns JSON-ответ
     * Поля возвращаемого JSON-объекта:
     * - pageCount - количество страниц всего
@@ -14,9 +15,24 @@
 
 include_once './../propel.inc.php';
 
+function alreadyInBasket($good, $col)
+{
+    //TODO написать нормальный (быстрый) поиск
+    foreach ($col as $current) {
+        if ($current["GoodId"] == $good->getId())
+            return true;
+    }
+    return false;
+}
+
 $cid = $_POST["categoryId"];
 $count = $_POST["count"];
 $skip = $_POST["skip"];
+$ssid = $_POST["sessionId"];
+
+$basket = BasketQuery::create()->findOneBySessionId($ssid);
+
+$goodsinbasket = $basket->getGoodInBaskets()->toArray();
 
 $goodsCount = GoodsQuery::create()
     ->findByCatalogId($cid)
@@ -40,10 +56,14 @@ $goodsResponse = '"goods":[';
 $firsttime = true;
 for ($i = $skip; $i < ($count + $skip) && $i < ($goodsCount + $skip); $i++) {
     $good = $goods[$i];
+    $contains = alreadyInBasket($good, $goodsinbasket) ? "true" : "false";
     if ($firsttime)        
         $firsttime = false;
     else $goodsResponse .= ",";
+    $con = ',"already":'.$contains;
     $goodsResponse .= $good->toJSON();
+    $goodsResponse = substr($goodsResponse, 0, strlen($goodsResponse) - 1);
+    $goodsResponse .= $con.'}';
 }
 $goodsResponse .= ']';
 
