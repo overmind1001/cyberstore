e8fe46bfc9aeae740150bcdb80c30fbf7bddb032
overmind1001@gmail.com <overmind1001@gmail.com@3422b5e4-9365-fb06-c293-7bd22532eeee>
2012-03-15 10:28:18 +0000

    Подправил главную

--- trunk/cyberstore.ru/www/index.php
+++ trunk/cyberstore.ru/www/index.php
@@ -1,9 +1,7 @@
-<!--
-<?
+<?php
     include_once 'findBasket.php';
     $basket = findBasket();
 ?>
--->
 
 <!DOCTYPE html>
 <html>
@@ -44,108 +42,108 @@
                 <div data-role="header" data-theme="c">
                     <table style="background:url('./logo.png');" padding="0" margin="0" border="0px" width="100%">
                         <tr>
-                        <td rowspan="2" style="width:35%; height:130px;">
-                            <!--img src="logo.gif"-->
+                            <td rowspan="2" style="width:35%; height:130px;">
+                                <!--img src="logo.gif"-->
                             </td>
                             <td valign="top" align="right">
                                 <a href="./login/" data-role="button" data-inline="true">Вход</a>
                             </td>
                         </tr>
-                            <td align="right" valign="bottom">
-                                <!--a id="basket" href="#" data-role="button" data-inline="true">Корзина: 0 товаров на 0 квазибит</a-->
-                            </td>
-                        <tr>
-                        </td>
+                        <!--td align="right" valign="bottom">
+                            <!--a id="basket" href="#" data-role="button" data-inline="true">Корзина: 0 товаров на 0 квазибит</a-->
+                        <!--/td-->
+                        <!--tr-->
+                        <!--/td-->
                     </table>
-                            <div data-role="navbar" >
-                                <ul>
-                                <li>
-                                    <a id="link1" href="#" data-theme="a">Главная</a>
-                                </li>
-                                <li>
-                                    <a id="link2" href="" onclick="$.mobile.changePage('./catalog/index.php'); $('#link2').addClass('ui-btn-active'); $('#link1').removeClass('ui-btn-active');" data-theme="a">Каталог</a>
-                                </li>
-                                <li>
-                                    <a href="#" data-theme="a">Корзина</a>
-                                </li>
-                                <li>
-                                    <a href="#" data-theme="a">Помощь</a>
-                                </li>
-                            </ul>
-                        </div>
+                    <div data-role="navbar" >
+                        <ul>
+                            <li>
+                                <a id="link1" href="#" data-theme="a">Главная</a>
+                            </li>
+                            <li>
+                                <a id="link2" href="" onclick="$.mobile.changePage('./catalog/index.php'); $('#link2').addClass('ui-btn-active'); $('#link1').removeClass('ui-btn-active');" data-theme="a">Каталог</a>
+                            </li>
+                            <li>
+                                <a href="#" data-theme="a">Корзина</a>
+                            </li>
+                            <li>
+                                <a href="#" data-theme="a">Помощь</a>
+                            </li>
+                        </ul>
                     </div>
-                    <!-- /header -->
+                </div><!-- /header -->
                     
-                    <div data-role="content">
-                        <p>
-                            <?php 
-                                if($basket==NULL) {
-                                    echo "Корзина = NULL";
+                    
+                <div data-role="content">
+                    <p>
+                        <?php 
+                            if($basket==NULL) {
+                                echo "Корзина = NULL";
+                            }
+                            else {
+                                echo " Сессия: ".$basket->getSessionId(). " Корзина: ".$basket->getId()." ";
+                                $user_id = $basket->getUserId();
+                                $user = UserQuery::create()->findOneById($user_id);
+                                if($user==NULL){
+                                    echo "Пользователь=NULL";
                                 }
                                 else {
-                                    echo " Сессия: ".$basket->getSessionId(). " Корзина: ".$basket->getId()." ";
-                                    $user_id = $basket->getUserId();
-                                    $user = UserQuery::create()->findOneById($user_id);
-                                    if($user==NULL){
-                                        echo "Пользователь=NULL";
-                                    }
-                                    else {
-                                        echo "Пользователь: ".$user->getLogin();
-                                    }
+                                    echo "Пользователь: ".$user->getLogin();
                                 }
-                            ?>
-                        </p>
-                        
-                        <table  class="startTable">
-                            <!--tr>
-                                <td>Изображение</td><td>Название</td><td>Описание</td>
-                            </tr-->
-                            <?php
-                                
-                                function printRow($good) {
-                                    $name=$good->getname();
-                                    $description=$good->getDescription();
-                                    $price=$good->getPriceCurrent();
-                                    
-                                    if($good->getPictureId()!=NULL){
-                                        $picture_path = './pictures/'."m".$good->getPictureId().'.jpg';
-                                    }
-                                    else{
-                                        $picture_path='./pictures/m0.jpg';
-                                    }
-                                    
-                                    echo "<tr>";
-                                    
-                                    echo "
-                                    <td class='pic'>
-                                        <table style='align: center;'>
-                                            <tr><td class='mainLeft'><img src='$picture_path'> </td></tr>
-                                            <tr><td class='mainLeft'>$name </td></tr>
-                                            <tr><td class='mainLeft'>Цена: $price квазибит</td></tr>
-                                        </table>
-                                    </td>
-                                    <td class='descr'>$description</td>";
-                                    
-                                    echo '</tr>';
-                                }
-                                
-                                $goods=GoodsQuery::create()->limit(3)->find();
-                                foreach ($goods as $good) {
-                                    printRow($good);
-                                }
-                            ?>
-                        </table>
-                    </div>
-                                    <!-- /content -->
+                            }
+                        ?>
+                    </p>
 
-    <!--div data-role="footer" data-position="fixed" data-theme="c">
-        copyright
-    </div-->
+                    <table  class="startTable">
+                        <!--tr>
+                            <td>Изображение</td><td>Название</td><td>Описание</td>
+                        </tr-->
+                        <?php
 
-</div><!-- /page -->
-<div data-role="footer">
-    <h1>© ДаЁжСофт</h1>
-</div><!-- #footer -->
-</div><!-- /wrapper-->
-</body>
+                            function printRow($good) {
+                                $name=$good->getname();
+                                $description=$good->getDescription();
+                                $price=$good->getPriceCurrent();
+
+                                if($good->getPictureId()!=NULL){
+                                    $picture_path = './pictures/'."m".$good->getPictureId().'.jpg';
+                                }
+                                else{
+                                    $picture_path='./pictures/m0.jpg';
+                                }
+
+                                echo "<tr>";
+
+                                echo "
+                                <td class='pic'>
+                                    <table style='align: center;'>
+                                        <tr><td class='mainLeft'><img src='$picture_path'> </td></tr>
+                                        <tr><td class='mainLeft'>$name </td></tr>
+                                        <tr><td class='mainLeft'>Цена: $price квазибит</td></tr>
+                                    </table>
+                                </td>
+                                <td class='descr'>$description</td>";
+
+                                echo '</tr>';
+                            }
+
+                            $goods=GoodsQuery::create()->limit(3)->find();
+                            foreach ($goods as $good) {
+                                printRow($good);
+                            }
+                        ?>
+                    </table>
+                </div>
+                                <!-- /content -->
+
+                <!--div data-role="footer" data-position="fixed" data-theme="c">
+                    copyright
+                </div-->
+
+            </div><!-- /page -->
+            <div data-role="footer">
+                <h1>© ДаЁжСофт</h1>
+            </div><!-- #footer -->
+        </div><!-- /wrapper-->
+    </body>
 </html>
