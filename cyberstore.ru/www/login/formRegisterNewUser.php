994a50a50fd1f5c073e9b89d38430cc69efa7104
overmind1001@gmail.com <overmind1001@gmail.com@3422b5e4-9365-fb06-c293-7bd22532eeee>
2012-03-19 13:15:20 +0000

    Перевел логин на новый интерфейс

--- cyberstore.ru/www/login/formRegisterNewUser.php
+++ cyberstore.ru/www/login/formRegisterNewUser.php
@@ -1,102 +1,83 @@
+<?php
+    include_once '../findBasket.php';
+    $basket = findBasket();
+?>
+
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Авторизация</title>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Cyberstore - регистрация нового пользователя</title>
+    <link href="../style.css" rel="stylesheet" type="text/css" />
+    <link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" rel="stylesheet" type="text/css"/>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
 
-        <link type="text/css" href="../css/black-tie/jquery-ui-1.8.18.custom.css" rel="stylesheet" />
-        <script type="text/javascript" src="../js/jquery-1.7.1.min.js"></script>
-        <script type="text/javascript" src="../js/jquery-ui-1.8.18.custom.min.js"></script>
+    <script>
+    $(function(){
+                $("input:button,input:submit,input:reset,button").button();
+            })
+    </script>
+</head>
+<!--
+<?
+    include '../propel.inc.php';
+?>
+-->
 
-        <link rel="stylesheet" href="http://code.jquery.com/mobile/1.0.1/jquery.mobile-1.0.1.min.css" />
-        <script src="../jquery.mobile-1.0.1.min.js"></script>
+<body>
+    <div id="wrap">
+        <div id="header">
+            <div id="enterbtn" style="float:right; padding:10px;">
+                <!--a href="./login/">Вход</a-->
+            </div>
+            <div id="basketinfo" style="float:right; margin-top:120px; margin-right:-43px;">
+                Корзина: 0 товаров на 0 кб сделать
+            </div>
+            <div id="nav">
+                <div id="topmenu">
+                    <ul>
+                        <li><a href="../">Главная</a></li>
+                        <li><a href="../catalog/">Каталог</a></li>
+                        <li><a href="../basket">Корзина</a></li>
+                        <li><a href="../about">Помощь</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="clear"></div>
+        <div id="content" style="">
+            <center>
+            <h1 align="center">Регистрация нового пользователя</h1>
+            <form method="POST" action="registerNewUser.php">
+                <table  align="center">
+                    <tr>
+                        <td>Логин:</td>
+                        <td><input type="text" name="login" required selected></td>
+                    </tr>
+                    <tr>
+                        <td>Пароль:</td>
+                        <td><input type="password" name="password" required></td>
+                    </tr>
 
-        <script src="../catalog/catalog.js"></script>
-        <link rel="stylesheet" href="../main.css" />
-        <script>
-            $(function(){
-                $.mobile.defaultPageTransition = "fade";
-                $.mobile.ajaxLinksEnabled = false;
-
-                $( 'div' ).live( 'pageshow', function(event, ui){
-                    $('#accordion').accordion();
-                    $('#accordion').css('width','200px');
-                    $('#accordion').css('margin','10px');
-                    $('.ui-accordion-content').css('padding-top','5px');
-                    $('.ui-accordion-content').css('padding-bottom','5px');
-                    $('.ui-accordion-content').css('padding-left','15px');
-                    $('.ui-accordion-content').css('padding-right','15px');
-                });
-
-            });
-        </script>
-    </head>
-    <body>
-        <div id="wrapper">
-            <div id="mainpage">
-                <div data-role="header" data-theme="c">
-                    <table style="background:url('../logo.png');" padding="0" margin="0" border="0px" width="100%">
-                        <tr>
-                            <td rowspan="2" style="width:35%; height:130px;">
-                  
-                            </td>
-                            <td valign="top" align="right">
-                                <!--a href="#" data-role="button" data-inline="true">Вход</a-->
-                            </td>
-                        </tr>
-                            <td align="right" valign="bottom">
-                                <a href="#" data-role="button" data-inline="true">Корзина: 0 товаров на 0 руб.</a>
-                            </td>
-                        <tr>
+                    <tr>
+                        <td>   
+                            <input type="text" name="nextPage" hidden readonly>
                         </td>
-                    </table>
-                            <div data-role="navbar" >
-                                <ul>
-                                <li>
-                                    <a href="#" data-theme="a">Главная</a>
-                                </li>
-                                <li>
-                                    <a href="#" onclick="$.mobile.changePage('./catalog/index.php');" data-theme="a">Каталог</a>
-                                </li>
-                                <li>
-                                    <a href="#" data-theme="a">Корзина</a>
-                                </li>
-                                <li>
-                                    <a href="#" data-theme="a">Помощь</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <!-- /header -->
-                    
-                    <div data-role="content">
-                        <h1 align="center">Регистрация нового пользователя</h1>
-                        <form method="POST" action="registerNewUser.php">
-                            <table  align="center">
-                                <tr>
-                                    <td>Логин:</td>
-                                    <td><input type="text" name="login" required selected></td>
-                                </tr>
-                                <tr>
-                                    <td>Пароль:</td>
-                                    <td><input type="password" name="password" required></td>
-                                </tr>
-                                
-                                <tr>
-                                    <td>   
-                                        <input type="text" name="nextPage" hidden readonly>
-                                    </td>
-                                    <td>
-                                        <button type="submit" name="submit">Зарегистрировать</button>
-                                    </td>
-                                </tr>
-                        </table>
-                        </form>
-                    </div>
-                                    <!-- /content -->
-</div><!-- /page -->
-<div data-role="footer">
-    <h1>© ДаЁжСофт</h1>
-</div><!-- #footer -->
-</div><!-- /wrapper-->
+                        <td>
+                            <button type="submit" name="submit">Зарегистрировать</button>
+                        </td>
+                    </tr>
+            </table>
+            </form>
+            </center>
+        </div><!-- /content -->
+                                    
+        <div id="footer">
+            <div id="credits">
+                (с) ДаЁжСофт
+            </div>
+        </div>
+    </div><!-- wrap -->
 </body>
 </html>
