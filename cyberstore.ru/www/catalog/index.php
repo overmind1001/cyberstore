68a46bb06e7e5c61886a6404af5249aa1c8d859d
antonstorozhev@gmail.com <antonstorozhev@gmail.com@3422b5e4-9365-fb06-c293-7bd22532eeee>
2012-03-19 13:54:46 +0000

    Каталог на новый лад

--- cyberstore.ru/www/catalog/index.php
+++ cyberstore.ru/www/catalog/index.php
@@ -4,88 +4,44 @@
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>CyberStore - каталог</title>
-        <link rel="stylesheet" href="http://code.jquery.com/mobile/1.0.1/jquery.mobile-1.0.1.min.css" />
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Cyberstore - запчасти для всей семьи</title>
+    <link type="text/css" href="./../css/black-tie/jquery-ui-1.8.18.custom.css" rel="stylesheet" />
+    <script type="text/javascript" src="./../js/jquery-1.7.1.min.js"></script>
+    <script type="text/javascript" src="./catalog.js"></script>
+    <script type="text/javascript" src="./../js/jquery-ui-1.8.18.custom.min.js"></script>
+    <link href="./../style.css" rel="stylesheet" type="text/css" />
+    <script>
+        $(function(){
+            $('#accordion').accordion();
+        });
+    </script>
+</head>
 
-        <link type="text/css" href="./../css/black-tie/jquery-ui-1.8.18.custom.css" rel="stylesheet" />
-        <script type="text/javascript" src="./../js/jquery-1.7.1.min.js"></script>
-        <script type="text/javascript" src="./../js/jquery-ui-1.8.18.custom.min.js"></script>
-        
-        <script src="./../jquery.mobile-1.0.1.min.js"></script>
-        <script src="./catalog.js"></script>
-        <link rel="stylesheet" href="./../main.css" />
-        
-        <script>
-            function updateSelectedButtons(){
-                $('#link1,link2').removeClass('ui-btn-active');
-                $('#link2').addClass('ui-btn-active'); 
-            }
-            $(function(){
-                //$.mobile.defaultPageTransition = "fade";
-                //$.mobile.ajaxLinksEnabled = false;
-                updateSelectedButtons();
-                $( 'div' ).live( 'pageshow', function(event, ui){
-                    $('#accordion').accordion();
-                    $('#accordion').css('width','200px');
-                    $('#accordion').css('margin','10px');
-                    $('.ui-accordion-content').css('padding-top','5px');
-                    $('.ui-accordion-content').css('padding-bottom','5px');
-                    $('.ui-accordion-content').css('padding-left','15px');
-                    $('.ui-accordion-content').css('padding-right','15px');
-                    <?php
-                        if ($basket != null)
-                            echo 'updateBasketInfo_b('.$basket->getId().');';
-                    ?>
-                });
-            });            
-        </script>
-    </head>
-    
-    
-<?
-    require_once './../vendor/propel/runtime/lib/Propel.php';
-    Propel::init("./../cyberstore/build/conf/cyberstore-conf.php");
-    set_include_path("./../cyberstore/build/classes" . PATH_SEPARATOR . get_include_path());
-?>
 <body>
-    <div id="wrapper">
-        <div id="mainpage">
-            <div data-role="header" data-theme="c">
-                <table style="background:url('./../logo.png');" padding="0" margin="0" border="0px" width="100%">
-                    <tr>
-                        <td rowspan="2" style="width:35%; height:130px;">
-                            <!--img src="logo.gif"-->
-                        </td>
-                        <td valign="top" align="right">
-                            <a href="../login" data-role="button" data-inline="true">Вход</a>
-                        </td>
-                    </tr>
-                        <td align="right" valign="bottom">
-                            <a id="basket" href="#" data-role="button" data-inline="true">Корзина: 0 товаров на 0 квазибит</a>
-                        </td>
-                    <tr>
-                </table>
-                <div data-role="navbar" >
+    <div id="wrap">
+        <div id="header">
+            <div id="enterbtn" style="float:right; padding:10px;">
+                <a href="">Вход</a>
+            </div>
+            <div id="basketinfo" style="float:right; margin-top:120px; margin-right:-43px;">
+                Корзина: 0 товаров на 0 кб
+            </div>
+            <div id="nav">
+                <div id="topmenu">
                     <ul>
-                        <li>
-                            <!--a id="link1" href="#" onclick="$.mobile.changePage('./../index.php'); $('#link1').addClass('ui-btn-active'); $('#link2').removeClass('ui-btn-active');" data-theme="a">Главная</a-->
-                            <a id="link1" href="../" data-theme="a">Главная</a>
-                        </li>
-                        <li>
-                            <a id="link2" href="../catalog" data-theme="a">Каталог</a>
-                        </li>
-                        <li>
-                            <a id="3" href="../basket" data-theme="a">Корзина</a>
-                        </li>
-                        <li>
-                            <a id="4" href="../about" data-theme="a">Помощь</a>
-                        </li>
+                        <li><a href="./../">Главная</a></li>
+                        <li class="active"><a href="styles.html">Каталог</a></li>
+                        <li><a href="#">Корзина</a></li>
+                        <li><a href="./../about/">Помощь</a></li>
                     </ul>
                 </div>
-            </div><!-- /header -->
-            <div data-role="content">
-                <div id="catalog">
+            </div>
+        </div>
+        <div class="clear"></div>
+        <div id="content" style="">
+            <div id="catalog">
                     <table border="0px" width="100%">
                         <tr>
                             <td style="width:10%; vertical-align:top;" rowspan="3">
@@ -132,13 +88,13 @@
                         </tr>
                     </table>
                 </div>
-            </div><!-- /content -->
+        </div>
 
-            <div data-role="footer">
-                <h1>© ДаЁжСофт</h1>
-            </div><!-- /footer -->
+        <div id="footer">
+            <div id="credits">
+                (с) ДаЁжСофт
+            </div>
+        </div>
 
-        </div><!-- /mainpage -->
-    </div><!-- /wrapper-->
 </body>
 </html>
