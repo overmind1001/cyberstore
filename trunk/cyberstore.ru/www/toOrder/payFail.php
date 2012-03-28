86e76c366e5db8dc64ef41711d23c027b2797692
San4oKiselev <San4oKiselev@3422b5e4-9365-fb06-c293-7bd22532eeee>
2012-03-28 14:11:22 +0000

    Оплата через WebMoney

---
+++ trunk/cyberstore.ru/www/toOrder/payFail.php
@@ -0,0 +1,56 @@
+<?php
+    include_once './../findBasket.php';
+    $basket = findBasket();
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>Cyberstore - корзина</title>
+        <link href="./../style.css" rel="stylesheet" type="text/css" />
+        <link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" rel="stylesheet" type="text/css"/>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
+        <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
+        <script type="text/javascript" src="../catalog/catalog.js"></script>
+        
+        <script>
+            $(function(){
+                $("button").button();
+            });
+        </script>
+    </head>
+    
+    <body>
+        <div id="wrap">
+            
+                <div id="header">
+                    <div id="nav">
+                        <div id="topmenu">
+                            <table>
+                                <tr>
+                                    <td><a href="./../">Главная</a></td>
+                                    <td><a href="./../catalog/">Каталог</a></td>
+                                    <td><a href="../basket">Корзина</a></td>
+                                    <td><a href="./../about/">Помощь</a></td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>   
+                </div>   
+            
+                <div class="clear"></div>
+                <div id="content" style="">
+                        <center>
+                            <h1 align="center">К сожалению, оплатить не удалось</h1>
+                        </center>
+                </div>
+
+        <div id="footer">
+            <div id="credits">
+                (с) ДаЁжСофт
+            </div>
+        </div>
+   </div>
+</body>
+</html>
